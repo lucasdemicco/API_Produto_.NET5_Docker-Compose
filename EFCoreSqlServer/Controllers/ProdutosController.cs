e1b25c383094b715673201d2f25dce20ddb13ac2
lucasdemicco <lucasmicco@hotmail.com>
2022-01-08 12:32:20 -0300

    Update project

---
+++ EFCoreSqlServer/Controllers/ProdutosController.cs
@@ -0,0 +1,74 @@
+﻿using EFCoreSqlServer.Data;
+using EFCoreSqlServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EFCoreSqlServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ContextProdutos _context;
+        public ProdutosController(ContextProdutos context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProduct()
+        {
+            if (!ModelState.IsValid)
+                return StatusCode(statusCode: 404, "Produtos não encontrados");
+
+            return Ok(await _context.Produtos.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produto>> Create(Produto produto)
+        {
+            if(!ModelState.IsValid)
+                return StatusCode(statusCode: 500, "Erro para inserir na base de dados");
+
+            await _context.Produtos.AddAsync(produto);
+            await _context.SaveChangesAsync();
+            return Ok(produto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Produto>> Update(int id, Produto produtoFromJson)
+        {
+            if (!ModelState.IsValid)
+                return StatusCode(statusCode: 500, "Erro para consultar na base de dados");
+
+            var produtos = await _context.Produtos.FindAsync(id);
+
+            if(produtos == null)
+            {
+                return StatusCode(statusCode: 404, "Produtos não encontrados");
+            }
+
+            produtos.Name = produtoFromJson.Name;
+            produtos.Price = produtoFromJson.Price;
+            produtos.Description = produtoFromJson.Description;
+
+            await _context.SaveChangesAsync();
+            return Ok(produtos);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Produto>> Delete(int id)
+        {
+            var produtos = await _context.Produtos.FindAsync(id);
+
+            if (produtos == null)
+                return StatusCode(statusCode: 404, "produtos não encontrados");
+
+            _context.Remove(produtos);
+            await _context.SaveChangesAsync();
+            return Ok(produtos);
+        }
+    }
+}
---
+++ EFCoreSqlServer/Data/ContextProdutos.cs
@@ -0,0 +1,15 @@
+﻿using EFCoreSqlServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSqlServer.Data
+{
+    public class ContextProdutos : DbContext
+    {
+        public ContextProdutos(DbContextOptions<ContextProdutos> options) : base(options) 
+        { 
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
